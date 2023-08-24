f167279200645292c674ee215d1314c6dd5da17d
Niki <nikistefan95@gmail.com>
2023-08-24 19:15:36 +0300

    init shadcn and next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ThemeProvider from '@/components/theme-provider';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Comfortaa } from 'next/font/google';
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body className={comfortaa.className}>{children}</body>
+      <body className={comfortaa.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 };
---
+++ components/theme-provider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  children,
+  ...props
+}) => {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+};
+
+export default ThemeProvider;
